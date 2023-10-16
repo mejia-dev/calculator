2498adaf9538efc222b4e312b08045e51ddeba2c
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-16 13:32:26 -0700

    Show operations performed in single alert pop-up
    Co-authored-by: Ben Raynolds <bgraynolds@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,16 @@ function divide(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number: "));
 
-window.alert("The addition of your numbers is: " + add(number1, number2) + ". The subtraction of your numbers is: " + subtract(number1, number2) + ". The multiplication of your numbers is: " + multiply(number1, number2) + ". The division of your numbers is: " + divide(number1, number2) + ".")
-// window.alert("The subtraction of your numbers is: " + subtract(number1, number2));
-// window.alert("The multiplication of your numbers is: " + multiply(number1, number2));
-// window.alert("The division of your numbers is: " + divide(number1, number2));
+// window.alert("The addition of your numbers is: " + add(number1, number2) + ". The subtraction of your numbers is : " + subtract(number1, number2) + ". The multiplication of your numbers is: " + multiply(number1, number2) + ". The division of your numbers is: " + divide(number1, number2) + ".")
+
+
+
+// window.alert("The addition of your numbers is: " + add(number1, number2) + ". The subtraction of your numbers is : " + subtract(number1, number2) + ". The multiplication of your numbers is: " + multiply(number1, number2) + ". The division of your numbers is: " + divide(number1, number2) + ".")
+
+
+window.alert(
+  number1 + " + " + number2 + " = " + add(number1, number2) + ", " + 
+  number1 + " - " + number2 + " = " + subtract(number1, number2) + ", " +
+  number1 + " * " + number2 + " = " + multiply(number1, number2) + ", " +
+  number1 + " / " + number2 + " = " + divide(number1, number2) + ", "
+  )
