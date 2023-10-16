dd79726da65384084c5c17dbc17d6e03e346fe89
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-16 16:56:11 -0700

    WIP commit while adding more button functionality

--- js/calculator-gui.js
+++ js/calculator-gui.js
@@ -2,24 +2,14 @@ let inputVar = "";
 
 function appendValue(num1) {
   inputVar += num1.toString();
+  document.getElementById("inputBox").innerText=inputVar.toString();
 }
 
-function appendOne() {
-  let inputVar = inputVar.toString() + "1";
+function appendDecimal() {
+  if !inputVar.includes(".")
 }
 
-
-//calcKey functions
-// const appendZero = document.getElementsByClassName("calcKey0");
-// appendZero.addEventListener('click', typeZero);
-
-
-// const append1 = document.getElementById("calcKey1");
-// const append2 = document.getElementById("calcKey2");
-// const append3 = document.getElementbyId("calcKey3");
-// const append4 = document.getElementbyId("calcKey4");
-// const append5 = document.getElementbyId("calcKey5");
-// const append6 = document.getElementbyId("calcKey6");
-// const append7 = document.getElementbyId("calcKey7");
-// const append8 = document.getElementbyId("calcKey8");
-// const append9 = document.getElementbyId("calcKey9");
+function clearInput() {
+  inputVar = "";
+  document.getElementById("inputBox").innerText=inputVar.toString();
+}
