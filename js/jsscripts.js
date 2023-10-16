7685a999a4bd8fe0f7c7828b75b29632a04d24f2
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-16 11:30:35 -0700

    Update scripts.js to use window alerts to say functionality

--- js/jsscripts.js
+++
@@ -1,7 +0,0 @@
-function saySomething(whatToSay) {
-  window.alert(whatToSay);
-}
-
-function add(number1, number2) {
-  return number1 + number2;
-}
--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,7 @@ function saySomething(whatToSay) {
 function add(number1, number2) {
   return number1 + number2;
 }
+
+const result = add(3,5);
+const outputText = "The sum is " + result + ".";
+saySomething(outputText);
