3b172b4653593409604e17c860d41c4b33b7078a
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-16 16:21:21 -0700

    Add new js file to perform calculations using GUI defined in index.html

---
+++ js/calculator-gui.js
@@ -0,0 +1,25 @@
+let inputVar = "";
+
+function appendValue(num1) {
+  inputVar += num1.toString();
+}
+
+function appendOne() {
+  let inputVar = inputVar.toString() + "1";
+}
+
+
+//calcKey functions
+// const appendZero = document.getElementsByClassName("calcKey0");
+// appendZero.addEventListener('click', typeZero);
+
+
+// const append1 = document.getElementById("calcKey1");
+// const append2 = document.getElementById("calcKey2");
+// const append3 = document.getElementbyId("calcKey3");
+// const append4 = document.getElementbyId("calcKey4");
+// const append5 = document.getElementbyId("calcKey5");
+// const append6 = document.getElementbyId("calcKey6");
+// const append7 = document.getElementbyId("calcKey7");
+// const append8 = document.getElementbyId("calcKey8");
+// const append9 = document.getElementbyId("calcKey9");
