2f89c56cf9f15c2598e2fa2e24e10b69fae93103
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-17 15:59:58 -0700

    Add exception to backSpace() for decimal deletion. Previously, backspacing a decimal would be counted as backspacing an operator

--- js/calculator-gui.js
+++ js/calculator-gui.js
@@ -6,56 +6,56 @@ let numberOfNumbers = 1;
 // Data append functions
 
 function appendValue(num1) {
-  
-  if (inputVar.includes(0) && (inputVar.length < 2) ) {
+
+  if (inputVar.includes(0) && (inputVar.length < 2)) {
     inputVar = "";
     inputVar += num1.toString();
-    document.getElementById("inputBox").innerText=inputVar.toString();
+    document.getElementById("inputBox").innerText = inputVar.toString();
   }
   else {
     inputVar += num1.toString();
-    document.getElementById("inputBox").innerText=inputVar.toString();
+    document.getElementById("inputBox").innerText = inputVar.toString();
   }
 }
 
 function appendDecimal() {
-  if (inputVar.includes(".") && ((inputVar.split(".").length -1) === numberOfNumbers)) {
+  if (inputVar.includes(".") && ((inputVar.split(".").length - 1) === numberOfNumbers)) {
     "";
   }
   else {
     inputVar += ".";
-    document.getElementById("inputBox").innerText=inputVar.toString();
+    document.getElementById("inputBox").innerText = inputVar.toString();
   }
 
 }
+
 
 // Data removal functions
 
 function clearInput() {
   inputVar = "";
   inputVar = "0";
-  document.getElementById("inputBox").innerText=inputVar.toString();
+  document.getElementById("inputBox").innerText = inputVar.toString();
 }
 
 function backSpace() {
-  if (isNaN(parseInt(inputVar.slice(-1)))) {
-    window.alert("NOT A NUMBER!!!")
+  if (isNaN(parseInt(inputVar.slice(-1))) && !".") {
     inputVar = inputVar.substring(0, inputVar.length - 3);
     numberOfNumbers = (numberOfNumbers - 1);
-    document.getElementById("inputBox").innerText=inputVar.toString();
-      if (inputVar === "") {
-        inputVar = "0";
-        document.getElementById("inputBox").innerText=inputVar.toString();
-      }
+    document.getElementById("inputBox").innerText = inputVar.toString();
+    if (inputVar === "") {
+      inputVar = "0";
+      document.getElementById("inputBox").innerText = inputVar.toString();
+    }
   }
   else {
     inputVar = inputVar.substring(0, inputVar.length - 1);
-    document.getElementById("inputBox").innerText=inputVar.toString();
-      if (inputVar === "") {
-        inputVar = "0";
-        document.getElementById("inputBox").innerText=inputVar.toString();
-        numberOfNumbers = 1;
-      }
+    document.getElementById("inputBox").innerText = inputVar.toString();
+    if (inputVar === "") {
+      inputVar = "0";
+      document.getElementById("inputBox").innerText = inputVar.toString();
+      numberOfNumbers = 1;
+    }
   }
 
 }
@@ -64,24 +64,24 @@ function backSpace() {
 // Math Buttons
 function buttonAdd() {
   inputVar += " + ";
-  document.getElementById("inputBox").innerText=inputVar.toString();
+  document.getElementById("inputBox").innerText = inputVar.toString();
   numberOfNumbers = (1 + numberOfNumbers);
 }
 
 function buttonSubtract() {
   inputVar += " - ";
-  document.getElementById("inputBox").innerText=inputVar.toString();
+  document.getElementById("inputBox").innerText = inputVar.toString();
   numberOfNumbers = (1 + numberOfNumbers);
 }
 
 function buttonMultiply() {
   inputVar += " * ";
-  document.getElementById("inputBox").innerText=inputVar.toString();
+  document.getElementById("inputBox").innerText = inputVar.toString();
   numberOfNumbers = (1 + numberOfNumbers);
 }
 
 function buttonDivide() {
   inputVar += " / ";
-  document.getElementById("inputBox").innerText=inputVar.toString();
+  document.getElementById("inputBox").innerText = inputVar.toString();
   numberOfNumbers = (1 + numberOfNumbers);
 }
